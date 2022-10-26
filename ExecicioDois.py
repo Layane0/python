caa49bd1d9b7b8c2ff3442d2699730455da38a22
Layane0 <layanepenido@gmail.com>
2022-10-25 21:56:50 -0300

    ExercioDois

---
+++ ExecicioDois.py
@@ -0,0 +1,7 @@
+# Crie um programa que contenha as variáveis a,b.
+
+a:str = input("Digite um nome: ")
+print("O nome é " + str(a))
+
+b:int = input("Digite um número: ")
+print("O número é " + str(b))
