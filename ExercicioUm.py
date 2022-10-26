450180d2c03f86502c24a414d8de3f1d49c8b9d0
Layane0 <layanepenido@gmail.com>
2022-10-25 21:31:37 -0300

    Arquivo com Variaveis a,b,c,d.

---
+++ ExercicioUm.py
@@ -0,0 +1,18 @@
+#Crie um programa que contenha as variáveis a, b, c, d.
+
+a = 10
+b = 10.5
+c = "Cleber"
+d = True
+
+print("O Valor da variável a é: "+ str(a) +" e o tipo dela é")
+print(type(a))
+
+print("O Valor da variável b é: "+ str(b) +" e o tipo dela é")
+print(type(b))
+
+print("O Valor da variável a é: "+ str(c) +" e o tipo dela é")
+print(type(c))
+
+print("O Valor da variável a é: "+ str(d) +" e o tipo dela é")
+print(type(d))
