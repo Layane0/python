a25add638460fe3df0fd9c5c03ea5e345414597e
Layane0 <layanepenido@gmail.com>
2022-10-27 21:39:41 -0300

    atividadeseis

--- atividade_corrigida/atividadeseis.py
+++ atividade_corrigida/atividadeseis.py
@@ -1,5 +1,10 @@
 #Crie um programa para realizar as operações de uma tabuada de multiplicação.
 
-Tabuada = int(input("Tabuada do número, "))
-print("Digite o intervalo do inicio e fim da tabuada")
-print('*' * '*')
+print("Tabuada de mulyiplicação")
+numero=int(input("Qual número você quer saber a tabuada? "))
+inicio = int(input("Digite o inicio do intervalo da tabuada:"))
+fim = int(input("Digite o fim do intervalo da tabuada:"))
+
+print("Tabuada de", numero, "de", inicio, "até" fim)
+for 1 in range(inicio,fim):
+        print(numero,'*', i,"=",(numero*(i)))
