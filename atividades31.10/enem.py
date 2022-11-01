4dfa0c9278c17850ef0a73e56308b1e3b8b8f2ef
Layane0 <layanepenido@gmail.com>
2022-11-01 19:56:49 -0300

    enem

---
+++ atividades31.10/enem.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia um conjunto de nomes de 20 estudantes
+#inscritos na prova do ENEM. Com esses nomes, realizar uma
+#ordenação crescente usando uma função para facilitar a localização
+#do nome na lista que será afixada no quadro de avisos da escola.
+
+Lista = ["Thamyres" , "Layane" , "Ronei" ,"Gabriel" ,"Gustavo" , "Luciana", "Vanderlei", "Dayane", "Thalita", "Larissa", "Hudson", "Paula", "Paulo", "Fernando", "Fernanda", "Gabriela", "Luiz", "Yure", "Camila", "Milena"]
+print(sorted(Lista))
