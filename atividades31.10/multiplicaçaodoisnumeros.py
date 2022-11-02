b36dfe83098f00e1971bc19add931f0015708116
Layane0 <layanepenido@gmail.com>
2022-11-01 21:19:30 -0300

    multiplicaçaodoisnumeros

--- atividades31.10/multiplicaçaodoisnumeros.py
+++ atividades31.10/multiplicaçaodoisnumeros.py
@@ -1,2 +1,13 @@
 #Crie um programa com uma função para multiplicar dois números e o algoritmo mostrar o resultado.
 #Comando para criar uma função é:
+
+def sum(num1, num2):
+    return num1 * num2
+
+print("\tmultiplicação de dois números\n")
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+print("")
+print("O resultado da multiplicação é %d" % sum(num1, num2))
