99f1f9cc9e898e0e2ee9665ba1a2bbcf2bf70008
Layane0 <layanepenido@gmail.com>
2022-11-04 20:36:30 -0300

    recuperaçao

---
+++ atividaderecuperaçao/recuperaçao.py
@@ -0,0 +1,20 @@
+#Faça um programa para Ler 3 valores (considere que não serão informados valores iguais) e escrever qual é o maior deles. Ao final perguntar se deseja repetir a operação com novos valores.
+
+print ("Digite três valores diferentes")
+num1 = int(input("Digite o primeiro numero: "))
+num2 = int(input("Digite o segundo numero: "))
+num3 = int(input("Digite o terceiro numero: "))
+
+maior = num1
+
+if (num2 > maior):
+    maior = num2
+if (num3 > maior):
+    maior = num3
+print("O número maior é:", maior)
+
+continuar = input("Deseja continuar? ")
+while continuar == 's' :
+    print("você está continuando")
+else:
+    print("você saiu")
---
+++ atividadetreinamento.py
@@ -0,0 +1,10 @@
+#Crie um programa para calcular o peso ideal de pessoas
+#do sexo feminino F e masculino M para isso é necessário receber a altura da pessoa. O programa deverar repetir
+#até que o usuario decida encerrar o programa quando for perguntado ao final de cada calculo.
+#Deverá , mostrar o valor de peso ideal com duas casas decimais.
+
+sexo = input("Digite o sexo feminino ou masculino: ")
+altura = float(input("Digite a altura da pessoa: "))
+
+if(sexo == "feminino"):
+    print("oi")
