161dc181eeee32c59e8ef375812bb91b9e7a3d54
Layane0 <layanepenido@gmail.com>
2022-11-04 21:33:19 -0300

    recuperaçao

--- atividaderecuperaçao/recuperaçao.py
+++ atividaderecuperaçao/recuperaçao.py
@@ -1,20 +1,19 @@
 #Faça um programa para Ler 3 valores (considere que não serão informados valores iguais) e escrever qual é o maior deles. Ao final perguntar se deseja repetir a operação com novos valores.
 
-print ("Digite três valores diferentes")
-num1 = int(input("Digite o primeiro numero: "))
-num2 = int(input("Digite o segundo numero: "))
-num3 = int(input("Digite o terceiro numero: "))
+continuar = 'S'
+while continuar == 'S':
 
-maior = num1
+    print ("Digite três valores diferentes")
+    num1 = int(input("Digite o primeiro numero: "))
+    num2 = int(input("Digite o segundo numero: "))
+    num3 = int(input("Digite o terceiro numero: "))
 
-if (num2 > maior):
-    maior = num2
-if (num3 > maior):
-    maior = num3
-print("O número maior é:", maior)
+    maior = num1
 
-continuar = input("Deseja continuar? ")
-while continuar == 's' :
-    print("você está continuando")
-else:
-    print("você saiu")
+    if (num2 > maior):
+        maior = num2
+    if (num3 > maior):
+        maior = num3
+    print("O número maior é:", maior)
+
+    continua = input("Deseja continuar? s ou n").upper()
